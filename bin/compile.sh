12117136cbbe0cbfdc5e6eb18c863d26558e389a
Nathan Braswell <miloignis@gmail.com>
2016-04-14 12:35:01 -0400

    Finishing up

--- bin/compile.sh
+++ bin/compile.sh
@@ -7,15 +7,11 @@ while read LINE; do
     echo ${LINE} >> out.krak
 done
 echo ${LINE} >> out.krak
-/build/kraken /kraken/krakenGrammer.kgm ./out.krak out > throwaway_stdout
+#/build/kraken /kraken/krakenGrammer.kgm ./out.krak out > throwaway_stdout
+/kraken/kraken ./out.krak out > throwaway_stdout
 printf '\377' # 255 in octal
-echo "\n"
-echo "==============="
-echo "C.h Output:"
-echo "===============\n"
-cat out/out.h
 echo "\n"
 echo "==============="
 echo "C.c Output:"
 echo "===============\n"
-cat out/out.c
+cat out.krak.c
